a44a447f818aac42d65600d7c9990b4d46bcd345
Rogelio Orona <Rogelio_Orona@epam.com>
2017-10-16 15:59:53 -0500

    Add PrimitiveStreams

---
+++ src/com/company/PrimitiveStreams.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class PrimitiveStreams {
+
+    //Create a integer stream with a range between 1 and 10 and print the result
+    private void exercise1() {
+
+    }
+
+    //Create a double stream, sum all the elements and print the result
+    private void exercise2() {
+
+    }
+
+    //Map the length of each word from the list to an integer stream and print the result
+    private void exercise3() {
+
+    }
+
+
+    public static void main(String[] args) {
+        PrimitiveStreams demo = new PrimitiveStreams();
+        demo.runExercise();
+
+    }
+
+    private void runExercise() {
+        exercise1();
+        //exercise2();
+        //exercise3();
+    }
+}
