1bf5ad5de2a60f2f8c6e5ee8b5e2b0a324ab61b6
Rogelio Orona <Rogelio_Orona@epam.com>
2017-10-16 15:19:01 -0500

    Solution for RestrictAndFilter demo

--- src/com/company/RestrictAndFilter.java
+++ src/com/company/RestrictAndFilter.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RestrictAndFilter {
 
@@ -11,6 +12,11 @@ public class RestrictAndFilter {
         List<String> list = Arrays.asList(
                 "The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
 
+        List<String> newList = list.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        newList.forEach(System.out::println);
     }
 
     //Modify exercise 1 so that the new list only contains strings that have an
@@ -19,6 +25,12 @@ public class RestrictAndFilter {
         List<String> list = Arrays.asList(
                 "The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG");
 
+        List<String> newList = list.stream()
+                .filter(s -> s.length() % 2 != 0)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        newList.forEach(System.out::println);
     }
 
     //Join the second, third and forth strings of the list into a single string,
@@ -27,14 +39,29 @@ public class RestrictAndFilter {
         List<String> list = Arrays.asList(
                 "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
 
+        String resultingString = list.stream()
+                .skip(1)
+                .limit(3)
+                .collect(Collectors.joining("-"));
+
+        System.out.println(resultingString);
+
     }
 
-    //From the list, convert to lower-case and remove duplicates then sort in natural order.
+    //From the list, convert to upper case and remove duplicates then sort in natural order.
     //Print the resulting list.
     private void exercise4() {
         List<String> list = Arrays.asList(
                 "The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG",
                 "in", "jumped", "over", "the", "rush", "forest", "SEA", "Air");
+
+        List<String> resultingList = list.stream()
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+
+        resultingList.forEach(System.out::println);
 
     }
 
