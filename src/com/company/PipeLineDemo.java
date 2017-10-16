38979a3759ffb8779cbf40b1ead9554a1b46d43e
Rogelio Orona <Rogelio_Orona@epam.com>
2017-10-16 12:37:25 -0500

    Add PipeLineDemo

---
+++ src/com/company/PipeLineDemo.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class PipeLineDemo {
+
+    public static void main(String[] args) {
+        //Create an empty stream of string
+
+        //Create a single element stream of integer
+
+        //Create a stream of integers from an array
+
+        //Convert a list to stream
+
+        //Create a stream of random numbers
+
+        //Create a stream of odd numbers
+    }
+}
