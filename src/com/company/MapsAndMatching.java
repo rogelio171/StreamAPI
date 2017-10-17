806d4797b5383fa7ad8b55b049317a619dd54279
Rogelio Orona <Rogelio_Orona@epam.com>
2017-10-17 11:44:59 -0500

    Add MapsAndMatching solution

--- src/com/company/MapsAndMatching.java
+++ src/com/company/MapsAndMatching.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MapsAndMatching {
@@ -13,40 +15,58 @@ public class MapsAndMatching {
                 Arrays.asList("M1", "M2", "M3"),
                 Arrays.asList("N1", "N2", "N3"));
 
+        list.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
     }
 
-    //Convert Stream<String[]> to Stream<String>
+    //Convert Stream<String[][]> to Stream<String> and print the result
     private void exercise2() {
         String[][] data = new String[][]{{"a", "b", "c"}, {"d", "e"}, {"f", "g", "h"}};
+
+        Arrays.stream(data)
+                .flatMap(Arrays::stream)
+                .forEach(System.out::println);
+
     }
 
     //Find the first element of the stream
     private void exercise3() {
         Stream<String> s = Stream.of("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
 
+        s.findFirst().ifPresent(System.out::println);
     }
 
     //Find any element of the stream
     private void exercise4() {
-        Stream<String> s = Stream.of("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+        Stream<String> s = Stream.of("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog").parallel();
 
+        s.findAny().ifPresent(System.out::println);
     }
 
     //Check if all integers in a list are 7
     private void exercise5() {
         List<Integer> numbers = Arrays.asList(7,7,7,7,7,7,7,7);
 
+        boolean isAllMatch = numbers.stream().allMatch(n -> n == 7);
+        System.out.println(isAllMatch);
     }
 
     //Check if any integers in a list is a 9
     private void exercise6() {
         List<Integer> numbers = Arrays.asList(3,4,5,6,8,2,6,9,5,1,4,7);
 
+        boolean isAnyMatch = numbers.stream().anyMatch(n -> n == 9);
+        System.out.println(isAnyMatch);
     }
 
     //Check if no integers in a list is a 3
     private void exercise7() {
         List<Integer> numbers = Arrays.asList(3,4,5,6,8,2,6,9,5,1,4,7);
+
+        boolean isNoneMatch = numbers.stream().noneMatch(n -> n == 3);
+        System.out.println(isNoneMatch);
     }
 
     public static void main(String[] args) {
