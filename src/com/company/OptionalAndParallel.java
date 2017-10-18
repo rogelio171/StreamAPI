2120f490f4813fc6f9883300c169b527468e2a15
Rogelio Orona <Rogelio_Orona@epam.com>
2017-10-18 10:35:17 -0500

    Add OptionalAndParallel solution

--- src/com/company/OptionalAndParallel.java
+++ src/com/company/OptionalAndParallel.java
@@ -1,21 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class OptionalAndParallel {
 
     //Complete the function average to work when no parameters are passed to it
     private Optional<Double> average(int... values) {
+        if(values.length == 0) {
+            return Optional.empty();
+        }
 
-        return Optional.empty();
+        int sum = IntStream.of(values).sum();
+        return Optional.of((double) sum / values.length);
     }
 
     //Return the value of the list or return zero if the list is empty using Optional class
     private int lengthOfList(List<String> stringList) {
+        Optional<List<String>> listOptional = Optional.of(stringList);
 
-        return 0;
+        return listOptional
+                .map(List::size)
+                .orElse(0);
     }
 
     //Checks the average method
@@ -32,14 +41,14 @@ public class OptionalAndParallel {
         int size = lengthOfList(companyNames);
         System.out.println(size);
 
-        List<String> emptyList = null;
+        List<String> emptyList = new ArrayList<>();
         size = lengthOfList(emptyList);
         System.out.println(size);
     }
 
     //Print values of a parallelStream
     private void exercise3() {
-
+        IntStream.rangeClosed(1, 150).parallel().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
